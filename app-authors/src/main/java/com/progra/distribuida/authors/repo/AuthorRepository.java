6265e2b6caf0128084910f8b48a621ede938bb3c
Juan Estrella <jcestrella@uce.edu.ec>
2025-05-15 19:44:07 -0500

    Added other endpoints.

--- app-authors/src/main/java/com/progra/distribuida/authors/repo/AuthorRepository.java
+++ app-authors/src/main/java/com/progra/distribuida/authors/repo/AuthorRepository.java
@@ -5,6 +5,20 @@ import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.Optional;
+
 @ApplicationScoped
 public class AuthorRepository implements PanacheRepositoryBase<Author, Integer> {
+
+    public Optional<Author> update(Integer id, Author author) {
+        var obj = this.findByIdOptional(id);
+        if (obj.isEmpty()) {
+            return Optional.empty();
+        }
+
+        var authorObj = obj.get();
+        authorObj.setName(author.getName());
+        authorObj.setVersion(author.getVersion());
+        return Optional.of(authorObj);
+    }
 }
--- app-authors/src/main/java/com/progra/distribuida/authors/rest/AuthorRest.java
+++ app-authors/src/main/java/com/progra/distribuida/authors/rest/AuthorRest.java
@@ -38,4 +38,35 @@ public class AuthorRest {
         return authorRepository.findAll().list();
     }
 
+    @POST
+    public Response create(Author author) {
+        try {
+            authorRepository.persist(author);
+            return Response.status(Response.Status.CREATED).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Response update(@PathParam("id") Integer id, Author author) {
+        var obj = authorRepository.findByIdOptional(id);
+        if (obj.isEmpty()) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        authorRepository.update(id, author);
+        return Response.ok(obj.get()).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response delete(@PathParam("id") Integer id) {
+        var obj = authorRepository.findByIdOptional(id);
+        if (obj.isEmpty()) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        authorRepository.deleteById(id);
+        return Response.ok(obj.get()).build();
+    }
 }
